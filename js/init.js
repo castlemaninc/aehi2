a7966c1f9bfc8cc1a25554b5595ba28f92faa19d
Bryan Castleman <castlemaninc@gmail.com>
2020-08-15 19:51:49 -0700

    adding a different theme

---
+++ js/init.js
@@ -0,0 +1,13 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+
+    $(".metals-mobile-links").hide();
+    $("#metals-link-mobile").click(function(){
+      $(".metals-mobile-links").toggle(500);
+    });
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
