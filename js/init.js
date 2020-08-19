a44fecd097cb286be85042b82638354e4b69575f
Bryan Castleman <castlemaninc@gmail.com>
2020-08-19 13:14:14 -0700

    wrapped jquery in a document ready function in init.js

--- js/init.js
+++ js/init.js
@@ -1,15 +1,16 @@
-(function($){
-  $(function(){
-    $('.dropdown-trigger').dropdown();
-    $('.sidenav').sidenav();
-    $('.parallax').parallax();
 
-    $(".metals-mobile-links").hide();
-    $("#metals-link-mobile").click(function(){
-      $(".metals-mobile-links").toggle(500);
-    });
 
-    console.log("ready");
+$( document ).ready(function() {
+  console.log( "ready!" );
 
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
+  $('.dropdown-trigger').dropdown();
+  $('.sidenav').sidenav();
+  $('.parallax').parallax();
+
+  $(".metals-mobile-links").hide();
+
+  $("#metals-link-mobile").click(function(){
+    $(".metals-mobile-links").toggle(500);
+  });
+
+});
