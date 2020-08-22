d829665931cb74eea02e52a1fff8308d4eacfc57
Bryan Castleman <castlemaninc@gmail.com>
2020-08-21 18:35:33 -0700

    adding down arrow to metals link in mobile navigation

--- js/init.js
+++ js/init.js
@@ -7,11 +7,13 @@ $( document ).ready(function() {
   $('.sidenav').sidenav();
   $('.parallax').parallax();
 
+  
   $(".metals-mobile-links").hide();
 
   $("#metals-link-mobile").click(function(){
     $(".metals-mobile-links").toggle(500);
   });
+  
 
   $('.collapsible').collapsible();
   $('input#input_text, textarea#textarea2').characterCounter();
