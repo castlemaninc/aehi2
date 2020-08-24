cb41b6f4020237a0aa3551389e77a79b45e95419
Bryan Castleman <castlemaninc@gmail.com>
2020-08-24 11:42:20 -0700

    adding images to pages

--- js/init.js
+++ js/init.js
@@ -6,7 +6,8 @@ $( document ).ready(function() {
   $('.dropdown-trigger').dropdown();
   $('.sidenav').sidenav();
   $('.parallax').parallax();
-
+  $('.scrollspy').scrollSpy();  
+  
   
   $(".metals-mobile-links").hide();
 
@@ -17,4 +18,20 @@ $( document ).ready(function() {
 
   $('.collapsible').collapsible();
   $('input#input_text, textarea#textarea2').characterCounter();
+
+  $('.dropdown-trigger-2').dropdown({
+    inDuration: 300,
+    outDuration: 225,
+    constrain_width: false, // Does not change width of dropdown to that of the activator
+    hover: false, // Activate on hover
+    gutter: ($('.dropdown-content').width()*3)/2.5 + 5, // Spacing from edge
+    belowOrigin: false, // Displays dropdown below the button
+    alignment: 'left' // Displays dropdown with edge aligned to the left of button
+  });
+
+  
+  
+    
+  
+
 });
